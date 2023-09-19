7e5420a4800689cf8a03105b96ef4cb9db9c2b4b
IMF24 <caseylcox25@gmail.com>
2023-09-19 17:03:01 -0400

    Final clean up fixes

--- main.py
+++ main.py
@@ -130,6 +130,7 @@ def split_audio() -> None:
 
     SHUT.rmtree(f"{tempDir}/{useModel}", True)
     SHUT.rmtree(f"{tempDir}/modelo_final", True)
+    OS.remove(tempDir)
 
     if (splitDrums.get()) and (excludeOrigDrums.get()):
         add_output_msg("Excluding original drums file...")
